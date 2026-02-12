2ba086aeeea00d1fc74025c432df134d97750c6d
Alan Reis <alanreisanjo@gmail.com>
2026-02-12 11:09:10 -0300

    Ajuste na tipagem

--- src/types/repositories/can-read-id.interface.ts
+++ src/types/repositories/can-read-id.interface.ts
@@ -1,5 +1,9 @@
 import type { IEntity } from "@caffeine/entity/types";
+import type { t } from "@caffeine/models";
 
-export interface ICanReadId<EntityType extends IEntity> {
+export interface ICanReadId<
+	SchemaType extends t.TSchema,
+	EntityType extends IEntity<SchemaType>,
+> {
 	findById(id: string): Promise<EntityType | null>;
 }
--- src/types/repositories/can-read-slug.interface.ts
+++ src/types/repositories/can-read-slug.interface.ts
@@ -1,5 +1,9 @@
 import type { IEntity } from "@caffeine/entity/types";
+import type { t } from "@caffeine/models";
 
-export interface ICanReadSlug<EntityType extends IEntity> {
+export interface ICanReadSlug<
+	SchemaType extends t.TSchema,
+	EntityType extends IEntity<SchemaType>,
+> {
 	findBySlug(slug: string): Promise<EntityType | null>;
 }
